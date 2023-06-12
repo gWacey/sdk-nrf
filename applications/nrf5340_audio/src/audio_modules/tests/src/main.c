53c36e0fdeffbf1ca3ae427e44607193f01b7fb5
Kristoffer Rist Skøien <kristoffer.skoien@nordicsemi.no>
2023-06-12 12:53:00 +0200

    applications: nrf5340_audio: commit before move
    To be squashed
    
    Signed-off-by: Kristoffer Rist Skøien <kristoffer.skoien@nordicsemi.no>

--- applications/nrf5340_audio/src/audio_modules/tests/src/main.c
+++ applications/nrf5340_audio/src/audio_modules/tests/src/main.c
@@ -1,14 +1,19 @@
 /*
- * Copyright (c) 2020 Nordic Semiconductor ASA
+ * Copyright (c) 2023 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
 #include <zephyr/ztest.h>
 #include <errno.h>
+#include "amod_api.h"
 
-ZTEST(suite_a_mod, test_a_mod_init)
+ZTEST(suite_a_mod, test_a_mod_init_fail)
 {
+	// int ret;
+
+	// ret = amod_open(NULL, NULL, NULL, NULL, NULL);
+
 	ztest_test_pass();
 }
 
