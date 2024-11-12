bb9dfb8da936340dba9c703edee4b719a363a7ce
Graham Wacey <graham.wacey@nordicsemi.no>
2024-11-12 13:30:39 +0000

    applications: nrf5340_audio: LED module for kits with other LED setup
    	Commit of partial implementation, prior to seeing the light!
    
    Signed-off-by: Graham Wacey <graham.wacey@nordicsemi.no>

--- applications/nrf5340_audio/src/modules/led_nrf5340.h
+++ applications/nrf5340_audio/src/modules/led_nrf5340.h
@@ -30,91 +30,24 @@ enum led_color {
 };
 
 /**
- * @brief Structure to define an LED units features.
+ * @brief Private pointer to the LED module's device.
  */
-struct led_unit_info {
-	/* LED's label. */
-	const char *label;
-	/* Index of the LED on the controller. */
-	uint32_t index;
-	/* Number of colors per LED. */
-	uint8_t num_colors;
-	/* Mapping of the LED colors. */
-	const uint8_t *color_mapping;
-};
-
-/**
- * @brief Structure to define an LED units current control information.
- */
-struct led_unit_ctrl {
-	/* Flag to indicate if the unit is on or off. */
-	bool on;
-	/* Flag to indicate if the unit is blinking. */
-	bool blink;
-	/* Color of the unit. */
-	enum led_color color;
-	/* Work structure. */
-	struct k_work *work;
-	/* Pointer to a timer. */
-	struct k_timer led_timer;
-};
-
-/**
- * @brief Structure to define an LED units current configuration.
- */
-struct led_unit_cfg {
-	/* Number of LEDs in the unit. */
-	size_t leds_num;
-	/* LED's unit features information. */
-	struct led_unit_info info;
-	/* LED's unit current control information. */
-	struct led_unit_ctrl ctrl;
-	/* LED's unit GPIO specifications. */
-	const struct gpio_dt_spec *color[COLORS_MAX_NUM];
-};
-
-/**
- * @brief Structure to define the LED controller's configuration.
- */
-struct led_config {
-	/* Number of LEDs in DT. */
-	size_t leds_num;
-	/* LED label from DT. */
-	const char *led_labels;
-	/* GPIO specification. */
-	const struct gpio_dt_spec *led_gpios;
-	/* Number of LED units. */
-	size_t led_units_num;
-	/* LED unit configurations. */
-	struct led_unit_cfg led_units[LED_UNIT_NUM];
-};
-
-/**
- * @brief Callback API for writing a strip of LED channels.
- */
-typedef void (*led_blink_work_handler)(struct k_work *work);
+extern const struct device *led_app_dev;
 
 /**
  * @brief LED description structure.
  */
 struct led_description {
 	/* LED label. */
-	char *label;
+	const char *label;
 	/* Mapping of LED to GPIO specification. */
 	uint8_t mapping;
 };
 
 /**
- * @brief Structure to define LED unit blink function table.
- */
-struct led_unit_blink_work_handler {
-	led_blink_work_handler blink_work;
-}
-
-/**
  * @brief Base blinking function.
  */
-void led_unit_blink(uint8_t led_unit)
+void led_unit_blink(uint8_t led_unit);
 
 /**
  * @brief Select LED configuration for the given board.
