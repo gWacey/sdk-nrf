2af628edf57f38b1941eaad0aad0a9e693099767
Graham Wacey <graham.wacey@nordicsemi.no>
2025-12-29 15:07:56 +0000

    applications: nrf5340_audio: Error -22 on reboots
    Occasionally during reboots, there will be an error
    -22 as headset and/or gateway initializes. When this
    happens on the Gateway, an <err> print will be generated.
    A headset usually reports a warning in the same situation
    due to PACs.
    
    This has, for instance, been observed in a unicast
    test-case with one gateway and one headset, where both
    are halted and rebooted (using pinreset) simultaneously
    multiple times.
    
    This would appear to be that the gateway is trying to
    stop a stream in a NULL unicast group (due to the reset).
    It should be possible to store the state of the stream
    state and only stop if playing (quick and tested to work
    on this branch). Alternatively, do not perform any media
    commands until the stream is configured, but what to do
    with them is interesting.
    
    Signed-off-by: Graham Wacey <graham.wacey@nordicsemi.no>

--- applications/nrf5340_audio/src/bluetooth/bt_content_control/bt_content_ctrl.c
+++ applications/nrf5340_audio/src/bluetooth/bt_content_control/bt_content_ctrl.c
@@ -19,20 +19,32 @@ LOG_MODULE_REGISTER(bt_content_ctrl, CONFIG_BT_CONTENT_CTRL_LOG_LEVEL);
 ZBUS_CHAN_DEFINE(cont_media_chan, struct content_control_msg, NULL, NULL, ZBUS_OBSERVERS_EMPTY,
 		 ZBUS_MSG_INIT(0));
 
+static enum content_control_evt_type state;
+
 static void media_control_cb(bool play)
 {
 	int ret;
 	struct content_control_msg msg;
 
 	if (play) {
-		msg.event = MEDIA_START;
+		if (state == MEDIA_STOP) {
+			msg.event = MEDIA_START;
+		} else {
+			return;
+		}
 	} else {
-		msg.event = MEDIA_STOP;
+		if (state == MEDIA_START) {
+			msg.event = MEDIA_STOP;
+		} else {
+			return;
+		}
 	}
 
 	ret = zbus_chan_pub(&cont_media_chan, &msg, K_NO_WAIT);
 	if (ret) {
 		LOG_ERR("zbus publication failed: %d", ret);
+	} else {
+		state = msg.event;
 	}
 }
 
@@ -134,6 +146,8 @@ int bt_content_ctrl_uuid_populate(struct net_buf_simple *uuid_buf)
 int bt_content_ctrl_init(void)
 {
 	int ret;
+
+	state = MEDIA_START;
 
 	if (IS_ENABLED(CONFIG_BT_MCS)) {
 		ret = bt_content_ctrl_media_server_init(media_control_cb);
